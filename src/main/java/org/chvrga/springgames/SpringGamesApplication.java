39943bf4795042b9ea35a1720a624ee367ef6c79
Ivana Frankic <chvrga@gmail.com>
2017-10-28 19:07:33 +0200

    initial commit

---
+++ src/main/java/org/chvrga/springgames/SpringGamesApplication.java
@@ -0,0 +1,12 @@
+package org.chvrga.springgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGamesApplication.class, args);
+	}
+}
