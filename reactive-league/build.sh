f2b5f6d07856b21faef9b281910ff108864da041
Ivana Frankic <chvrga@gmail.com>
2017-11-04 12:42:27 +0100

    travis + codecov

--- reactive-league/build.sh
+++ reactive-league/build.sh
@@ -1,33 +1,7 @@
 #!/bin/sh
 
-mvn clean package
-ret=$?
-if [ $ret -ne 0 ]; then
-	exit $ret
-fi
-rm -rf target
-
 ./gradlew build
-ret=$?
-if [ $ret -ne 0 ]; then
-	exit $ret
-fi
-rm -rf build
 
-cd ../initial
-
-mvn clean compile
-ret=$?
-if [ $ret -ne 0 ]; then
-	exit $ret
-fi
-rm -rf target
+mvn clean install -Pqulice
 
 ./gradlew compileJava
-ret=$?
-if [ $ret -ne 0 ]; then
-	exit $ret
-fi
-rm -rf build
-
-exit
